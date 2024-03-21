29f5dcf364ea097c7f0c846d99172ecd40d318da
VictorChefala <victor.chefala@iis.utm.md>
2024-03-21 11:05:51 +0200

    lab_3

--- antd-app/src/components/FormComponent.js
+++ antd-app/src/components/FormComponent.js
@@ -1,30 +1,138 @@
-import React from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, Button, Modal } from 'antd';
 
 const FormComponent = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     const onFinish = values => {
-        console.log('Form submitted:', values); // Afisare in consola
+        console.log('Form submitted:', values); // Afișare în consolă
+        setIsModalVisible(true); // Afisare caseta de dialog
+    };
+
+    const handleModalOk = () => {
+        console.log('User confirmed action');
+        setIsModalVisible(false); // Inchidere caseta de dialog
+        // Aici puteți executa alte acțiuni după ce utilizatorul a confirmat
         alert('Form submitted successfully!'); // Afișare alertă
     };
 
-    return (
+    const handleModalCancel = () => {
+        console.log('User canceled action');
+        setIsModalVisible(false); // Inchidere caseta de dialog
+        // Aici puteți executa alte acțiuni după ce utilizatorul a anulat
+    };
 
-        <Form onFinish={onFinish} style={{ maxWidth: 300 }}>
-            <Form.Item name="Card Holder">
-                <Input placeholder="Card Holder" />
-            </Form.Item>
-            <Form.Item name="Name of bank">
-                <Input placeholder="Name of bank" />
-            </Form.Item>
-            <Form.Item name="Number of card">
-                <Input placeholder="Number of card" />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+    return (
+        <div>
+            <Form onFinish={onFinish} style={{ maxWidth: 300 }}>
+                <Form.Item
+                    name="CardHolder"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input card holder information!',
+                        },
+                        {
+                            pattern: /^[A-Z\s]+$/,
+                            message: 'Please input only uppercase letters and spaces!',
+                        },
+                    ]}
+                >
+                    <Input placeholder="Card Holder" />
+                </Form.Item>
+                <Form.Item
+                    name="CardNumber"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input card number!',
+                        },
+                        {
+                            pattern: /^[0-9]+$/,
+                            message: 'Please input only numbers!',
+                        },
+                        {
+                            len: 16,
+                            message: 'Please input exactly 16 digits for card number!',
+                        },
+                    ]}
+                >
+                    <Input placeholder="Card Number (16 digits)" />
+                </Form.Item>
+                <Form.Item
+                    name="ExpirationDate"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input expiration date!',
+                        },
+                        {
+                            pattern: /^[0-9/]+$/,
+                            message: 'Please input only numbers and /!',
+                        },
+                        {
+                            len: 5,
+                            message: 'Please input exactly 5 characters for expiration date (MM/YY)!',
+                        },
+                    ]}
+                >
+                    <Input placeholder="Expiration Date (MM/YY)" />
+                </Form.Item>
+                <Form.Item
+                    name="PIN"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input PIN!',
+                        },
+                        {
+                            pattern: /^[0-9]+$/,
+                            message: 'Please input only numbers!',
+                        },
+                        {
+                            len: 4,
+                            message: 'Please input exactly 4 digits for PIN!',
+                        },
+                    ]}
+                >
+                    <Input.Password placeholder="PIN (4 digits)" />
+                </Form.Item>
+                <Form.Item
+                    name="CVV"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input CVV!',
+                        },
+                        {
+                            pattern: /^[0-9]+$/,
+                            message: 'Please input only numbers!',
+                        },
+                        {
+                            len: 3,
+                            message: 'Please input exactly 3 digits for CVV!',
+                        },
+                    ]}
+                >
+                    <Input.Password placeholder="CVV (3 digits)" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Modal
+                title="Confirmation"
+                visible={isModalVisible}
+                onOk={handleModalOk}
+                onCancel={handleModalCancel}
+                okText="Yes"
+                cancelText="No"
+            >
+                <p>Are you sure you want to submit the form?</p>
+            </Modal>
+        </div>
     );
 };
 
--- antd-app/src/components/MenuComponent.js
+++ antd-app/src/components/MenuComponent.js
@@ -5,8 +5,12 @@ const MenuComponent = () => {
     return (
         <Menu theme="dark" mode="horizontal" >
             <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Contacts</Menu.Item>
-            <Menu.Item key="3">About</Menu.Item>
+            <Menu.SubMenu key="2" title="Contacts">
+                <Menu.Item key="3">MAIB: 123-456-789</Menu.Item>
+                <Menu.Item key="4">MICB: 987-654-321</Menu.Item>
+                <Menu.Item key="5">VictoriaBank: 555-123-456</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.Item key="6">About</Menu.Item>
         </Menu>
     );
 };
